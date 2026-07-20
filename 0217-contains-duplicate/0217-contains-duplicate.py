c653a4d0e9646ef97f84be9a6408975fefd064a9
Ann Maria Siby <annmariasiby20@gmail.com>
2026-07-20 11:50:35 +0530

    Time: 19 ms (68.51%), Space: 25.8 MB (62.44%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        s=set(nums)
-        if len(s)==len(nums):
+        seen=set(nums)
+        if len(seen)==len(nums):
             return False
         return True        
