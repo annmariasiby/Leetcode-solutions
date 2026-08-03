1e28a89a6b1d1c191be7140bf1b69f9c647cf551
Ann Maria Siby <annmariasiby20@gmail.com>
2026-08-03 14:34:03 +0530

    Time: 764 ms (46.38%), Space: 18.3 MB (80.6%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        answer=[]
+       
+        for i in range(0,len(nums)-2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j=i+1
+            k=len(nums)-1
+            while(j<k):
+                tnums=nums[i]+nums[k]+nums[j]
+                if(tnums==0):
+                    answer.append([nums[i],nums[k],nums[j]])
+                    j=j+1
+                    k=k-1
+                    while(j<k and (nums[j]==nums[j-1])):
+                        j=j+1
+                    while(j<k and (nums[k]==nums[k+1])):
+                        k=k-1
+                elif(tnums<0):
+                    j=j+1
+                   
+                else:
+                    k=k-1
+        return answer
