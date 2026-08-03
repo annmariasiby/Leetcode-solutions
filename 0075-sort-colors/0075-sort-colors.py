d2b338ab536ce375ff3389f4b71b180daa1ce7e2
Ann Maria Siby <annmariasiby20@gmail.com>
2026-08-03 15:05:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (56%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def sortColors(self, nums):
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<=high):
+            if(nums[mid]==0):
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low=low+1
+                mid=mid+1
+            elif(nums[mid]==1):
+                mid=mid+1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high=high-1
+        return nums
